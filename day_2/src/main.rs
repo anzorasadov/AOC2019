59800c118ce9f42c2cfea1301e0c3dd3ec5cb1de
Anzor <anzor.asadov@sclable.com>
2019-12-04 11:55:46 +0100

    day two part 2

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -2,10 +2,38 @@ use std::fs;
 
 fn main() {
 
-  let _inputstring = fs::read_to_string("input.txt")
-    .expect("Something went wrong reading the file");
+  let _inputstring = fs::read_to_string("input.txt").expect("Something went wrong reading the file");
+  let input: Vec<_> = _inputstring.split(",").map(|x| x.parse::<i32>().unwrap()).collect();
 
-  let mut input: Vec<_> = _inputstring.split(",").map(|x| x.parse::<i32>().unwrap()).collect();
+  part_one(input.clone());
+  part_two(input.clone());
+
+}
+
+
+fn part_one(input: Vec<i32>) {
+  let response = process_input(input);
+  println!("part 1: {}", response.to_string());
+}
+
+fn part_two(mut input: Vec<i32>) {
+  let result_val = 19690720;
+  
+  for i in 1..100 {
+    for j in 0..100 {
+      input[1] = i;
+      input[2] = j;
+      let result = process_input(input.clone());
+      
+      if result == result_val {
+        let answer = 100 * i + j;
+        println!("part 2: {}", answer);
+      }
+    }
+  }
+}
+
+fn process_input(mut input: Vec<i32>) -> i32 {
   let input_size = input.len();
   
   for i in (0..input_size as i32).step_by(4) {
@@ -37,6 +65,5 @@ fn main() {
     }
   }
 
-  println!("part 1: {}", input[0]);
-
+  return input[0];
 }
