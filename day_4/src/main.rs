30f61110d1ce8408e609d1f1c9bc2df9e68f3e60
Anzor <anzor.asadov@sclable.com>
2019-12-06 09:59:42 +0100

    day 4 part 2

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -1,27 +1,55 @@
 fn main() {
 	let min = 138241;
 	let max = 674034;
-	let mut possible_combinations: Vec<i32> = Vec::new();
+	let mut possible_combinations_one: Vec<i32> = Vec::new();
+	let mut possible_combinations_two: Vec<i32> = Vec::new();
+
 
 	for num in min..max {
 
 		let adjacent_exists = find_adjacent(num.clone());
 		let is_asc = get_is_asc(num.clone());
+		let two_adjacent_exists = find_two_adjacent(num.clone());
 		
 		if adjacent_exists && is_asc {
-			possible_combinations.push(num);
+			possible_combinations_one.push(num);
+		}
+
+		if two_adjacent_exists && is_asc {
+			possible_combinations_two.push(num);
 		}
 
 	}
 
-	println!("part one: {}", possible_combinations.len());
+	println!("part one: {}", possible_combinations_one.len());
+	println!("part two: {}", possible_combinations_two.len());
 }
 
 fn find_adjacent(num: i32) -> bool {
 	let digits: Vec<char> = num.to_string().chars().collect();
+
 	for (idx, digit) in digits.iter().enumerate() {
-		if digits.len()-1 > idx && digits[idx+1] == *digit {
+		let next_adjacent = digits.len()-1 > idx && digits[idx+1] == *digit;
+		if next_adjacent {
 			return true;
+		}
+	}
+	return false;
+}
+
+fn find_two_adjacent(num: i32) -> bool {
+	let digits: Vec<char> = num.to_string().chars().collect();
+	let mut streak = 1;
+	
+	for (idx, digit) in digits.iter().enumerate() {
+		let next_adjacent = digits.len()-1 > idx && digits[idx+1] == *digit;
+		
+		if next_adjacent {
+			streak = streak+1;
+		} else if streak == 2 {
+			return true;
+		} else {
+			streak = 1;
 		}
 	}
 	return false;
